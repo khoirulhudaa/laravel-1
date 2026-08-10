754978b11dbbe432a8fcdb1c014192aac403794b
dukungin <dukung.in.support@gmail.com>
2026-08-10 14:43:59 +0700

    ok

--- database/seeders/ProdukElektroniksSeeder.php
+++ database/seeders/ProdukElektroniksSeeder.php
@@ -2,15 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ProdukElektroniks;
 use Illuminate\Database\Seeder;
 use App\Models\ProdukElektroniks;
 
 class ProdukElektroniksSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         ProdukElektroniks::factory()->count(50)->create();
